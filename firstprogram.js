2047b2ed4be807dde4d63eb50ccc9e967c6f8cfd
sweta sriramadasu <swethasriramdas2311@gmail.com>
2022-05-02 16:42:16 +0200

    workspace day6

---
+++ firstprogram.js
@@ -0,0 +1,61 @@
+console.log("Hello!");
+let a=5;/*  using let variable*/
+let b =6;
+let add= a+b;
+console.log(add);
+let sub= a-b;
+console.log(sub);
+let mul= a*b;
+console.log(mul);
+let div=a/b;
+console.log(div);
+let inc =a++;
+console.log(inc);
+console.log(a);
+
+const price1 = 5;/*  using constant variable*/
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+var x = 10; /*  using var variable*/
+var y = 6;
+var z = x + y;
+console.log(z);
+
+
+const pi = 3.14;   /*  using Data types */
+let person = "sweta";
+var answer = 'Yes I am sweta!';
+console.log(pi);
+console.log(person);
+console.log(answer);
+
+
+var myVar = null; /*  using Null */
+console.log(null);
+
+var d;
+console.log(d);
+
+
+let g = 16 + 4 + "sweta";
+console.log(g);
+let f = "Volvo" + 16;
+console.log(f);
+
+
+let i;         // Now i is undefined
+i = 5;         // Now i is a Number
+i = "sai";
+console.log(i);
+
+let ans1 = "happy birthday"; //adding 2 strings
+let ans2 = "sweta'";
+console.log(ans1+ans2);
+
+
+
+
+
+  
